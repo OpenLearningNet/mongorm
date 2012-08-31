dd486dcc94e00fa0e3b532f42dc7713a972f658d
John Garland <john@openlearning.com>
2012-08-31 13:28:32 +1000

    Use a "constant" (i.e. flag) to check for pytz

--- mongorm/fields/DateTimeField.py
+++ mongorm/fields/DateTimeField.py
@@ -3,22 +3,24 @@ from mongorm.fields.BaseField import BaseField
 from datetime import datetime
 
 try:
-	from pytz import UTC
+	from pytz import utc
 except ImportError:
-	UTC = None
+	PYTZ = False
+else:
+	PYTZ = True
 
 class DateTimeField(BaseField):
 	def fromPython( self, pythonValue, dereferences=[], modifier=None ):
 		if pythonValue is not None and not isinstance(pythonValue, datetime):
 			raise ValueError, "Value must be a datetime object not %r" % (pythonValue,)
 
-		if UTC is not None and pythonValue is not None and pythonValue.tzinfo is not None:
-			pythonValue = pythonValue.astimezone( UTC )
+		if PYTZ and pythonValue is not None and pythonValue.tzinfo is not None:
+			pythonValue = pythonValue.astimezone( utc )
 			pythonValue = pythonValue.replace( tzinfo=None )
 
 		return pythonValue
 	
 	def toPython( self, bsonValue ):
-		if UTC is not None and bsonValue is not None:
-			bsonValue = UTC.localize( bsonValue )
+		if PYTZ and bsonValue is not None:
+			bsonValue = utc.localize( bsonValue )
 		return bsonValue
