b93fd1fa74e88e6b41bf09d93aa762eecde0e2ed
John Garland <john@openlearning.com>
2012-12-19 17:44:17 +1100

    Add replicaSet option to connect command

--- mongorm/connection.py
+++ mongorm/connection.py
@@ -26,7 +26,7 @@ def getConnection( ):
 	if connection is None:
 		connectionArgs = {}
 		
-		for key in ['host', 'port']:
+		for key in ['host', 'port', 'replicaSet']:
 			if key in connectionSettings:
 				connectionArgs[key] = connectionSettings[key]
 		
