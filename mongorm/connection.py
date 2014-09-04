b09960733fd3e55c2e940b36a93278f205254cf2
David Collien <david@openlearning.com>
2014-09-04 11:57:12 +1000

    drop indexes when unique=True and dropDups=True change

--- mongorm/connection.py
+++ mongorm/connection.py
@@ -42,15 +42,33 @@ def getConnection( ):
 def ensureIndexes( ):
 	global stagedIndexes, registeredIndexes
 
+	assert database is not None, "Must be connected to database before ensuring indexes"
+
 	# Ensure indexes on the documents
 	try:
 		for collection, key_or_list, kwargs in stagedIndexes:
+			if isinstance(key_or_list, basestring):
+				key = key_or_list
+
+				# if args on the index have changed, drop the index
+				indexInfo = database[collection].index_information().get(key, None)
+				if indexInfo is not None:
+					hasChanged = False
+					if kwargs.get('unique', False) or indexInfo.get('unique', False):
+						if kwargs.get('unique', False) != indexInfo.get('unique', False):
+							hasChanged = True
+						if kwargs.get('dropDups', False) != indexInfo.get('dropDups', False):
+							hasChanged = True
+
+					if hasChanged:
+						database[collection].drop_index(key)
+
 			database[collection].ensure_index(key_or_list, **kwargs)
 	except Exception as err:
 		raise err
 	else:
 		registeredIndexes += stagedIndexes
-		stagedIndexes = []		
+		stagedIndexes = []
 
 def getDatabase( ):
 	global database, connectionSettings
