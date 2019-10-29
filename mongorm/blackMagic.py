b6bd4cd395e1f7e4c9d8390786c5cc0952de0fb1
Paul Jukic <paul@openlearning.com>
2019-10-29 16:08:32 +1100

    Guard against futures newobject type that wraps object

--- mongorm/blackMagic.py
+++ mongorm/blackMagic.py
@@ -1,4 +1,5 @@
+from future.types.newobject import newobject
 
 def serialiseTypesForDocumentType( documentType ):
-	return [ cls.__name__ for cls in documentType.mro() if cls != object \
+	return [ cls.__name__ for cls in documentType.mro() if cls not in [object, newobject] \
 			 and cls.__name__ not in ['Document', 'BaseDocument', 'EmbeddedDocument'] ]
