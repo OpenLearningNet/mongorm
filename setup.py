5aa92fa52632cec5e88e67bb3b0843b322f27e56
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2020-06-12 18:10:46 +1000

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mongorm',
-      version='0.5.9.2',
+      version='0.5.9.3',
       packages=find_packages(),
       author='Theo Julienne, John Garland',
       author_email='theo@icy.com.au, john@openlearning.com',
